470b24f2359b02c071b77bffb230531b0575bec9
fxf981 <yeyunet@gmail.com>
2024-03-17 09:11:02 +0800

    d

--- f.sh
+++ f.sh
@@ -3,32 +3,32 @@
 # Set config file
 cat <<EOF >/fei.json
 {
-    "log": {
-      "loglevel": "none"
-    },
-    "inbounds": [
-      {
-        "listen": "0.0.0.0",
-        "port": 8080,
-        "protocol": "vless",
-        "settings": {
-          "clients": [
-              {
-                "id": "471b2a75-a3a3-48d2-a5c9-9636fb92145f"
-              }
-          ],
-          "decryption": "none"
-        },
-        "streamSettings": {
-          "network": "ws"
-        }
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "listen": "0.0.0.0",
+      "port": 8080,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+            {
+              "id": "471b2a75-a3a3-48d2-a5c9-9636fb92145f"
+            }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws"
       }
-    ],
-    "outbounds": [
-        {
-          "protocol": "freedom"
-        }
-    ]
+    }
+  ],
+  "outbounds": [
+      {
+        "protocol": "freedom"
+      }
+  ]
 }
 EOF
 
