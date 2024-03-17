44eea1b733b84a0f261c94e9fbc9a0c67b1a72dd
fxf981 <yeyunet@gmail.com>
2024-03-17 09:03:04 +0800

    Update f.sh

--- f.sh
+++ f.sh
@@ -4,28 +4,29 @@
 cat <<EOF >/fei.json
 {
     "log": {
-        "loglevel": "none"
+      "loglevel": "none"
     },
     "inbounds": [
-        {
-            "listen": "0.0.0.0",
-            "port": 8080,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                      "id": "471b2a75-a3a3-48d2-a5c9-9636fb92145f"
-                    }
-                ],
-            },
-            "streamSettings": {
-              "network": "ws"
-            }
+      {
+        "listen": "0.0.0.0",
+        "port": 8080,
+        "protocol": "vless",
+        "settings": {
+          "clients": [
+              {
+                "id": "471b2a75-a3a3-48d2-a5c9-9636fb92145f"
+              }
+          ],
+          "decryption": "none"
+        },
+        "streamSettings": {
+          "network": "ws"
         }
+      }
     ],
     "outbounds": [
         {
-            "protocol": "freedom"
+          "protocol": "freedom"
         }
     ]
 }
