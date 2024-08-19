5087e67ff15ff3a6dc01b4ca02c62ddb2fb10287
fxf981 <105899270+fxf981@users.noreply.github.com>
2024-08-19 19:56:17 +0800

    Update f.sh

--- f.sh
+++ f.sh
@@ -7,15 +7,6 @@ cat <<EOF >/fei.json
   "log": {
     "loglevel": "info"
   },
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "network": "udp,tcp",
-        "outboundTag": "wireguard"
-      }
-    ]
-  },
   "inbounds": [
         {
             "listen": "0.0.0.0",
@@ -24,30 +15,18 @@ cat <<EOF >/fei.json
             "settings": {
                 "clients": [
                     {
-                        "id": "9c031bbc-482d-49dd-8383-5ba774ffd50c"
+                      "id": "9c031bbc-482d-49dd-8383-5ba774ffd50c"
                     }
                 ]
             },
             "streamSettings": {
-                "network": "ws",
-                "security": "none"
+                "network": "ws"
             }
         }
     ],
   "outbounds": [
     {
-      "protocol": "wireguard",
-      "settings": {
-        "secretKey": "uANyk/Mo95AsblVqXXxJYIbLNYfUvQI66NlSkBwQzHc=",
-        "address": ["172.16.0.2/32", "2606:4700:110:8949:fed8:2642:a640:c8e1/128"],
-        "peers": [
-          {
-            "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-            "endpoint": "engage.cloudflareclient.com:2408"
-          }
-        ]
-      },
-      "tag": "wireguard"
+      "protocol": "freedom"
     }
   ]
 }
