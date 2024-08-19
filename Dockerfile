b45755b18a64b1675bd38eed7f03ae5d744397db
fxf981 <105899270+fxf981@users.noreply.github.com>
2024-08-19 18:34:58 +0800

    t

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,23 @@ FROM debian:bookworm
 
 ADD f.sh /f.sh
 ADD xr /usr/local/bin/xr
+ADD geoip.dat /usr/local/bin/geoip.dat
+ADD geosite.dat /usr/local/bin/geosite.dat
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
     ca-certificates \
     bash \
     tor \
-    wireguard-tools \
-    iproute2 \
     gzip && \
     gzexe /f.sh && \
+    rm -rf /f.sh~ && \
     chmod +x /f.sh && \
     chmod +x /usr/local/bin/xr && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 
-
-ADD wgcf.conf /etc/wireguard/wgcf.conf
 
 
 CMD /f.sh
--- f.sh
+++ f.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 启动 WireGuard
-wg-quick up wgcf
 
 # Set config file
 cat <<EOF >/fei.json
@@ -9,27 +7,49 @@ cat <<EOF >/fei.json
   "log": {
     "loglevel": "none"
   },
-  "inbounds": [
-    {
-      "listen": "0.0.0.0",
-      "port": 8000,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-            {
-              "id": "471b2a75-a3a3-48d2-a5c9-9636fb92145f"
-            }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws"
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "type": "field",
+        "port": "0-65535",
+        "outboundTag": "wireguard-1"
       }
-    }
-  ],
+    ]
+  },
+  "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 8000,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "9c031bbc-482d-49dd-8383-5ba774ffd50c"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
   "outbounds": [
     {
-      "protocol": "freedom"
+      "protocol": "wireguard",
+      "settings": {
+        "secretKey": "uANyk/Mo95AsblVqXXxJYIbLNYfUvQI66NlSkBwQzHc=",
+        "address": ["172.16.0.2/32", "2606:4700:110:8949:fed8:2642:a640:c8e1/128"],
+        "peers": [
+          {
+            "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+            "endpoint": "engage.cloudflareclient.com:2408"
+          }
+        ],
+        "reserved":[0, 0, 0] // 如果你有的话，粘贴reserved到这里
+      },
+      "tag": "wireguard-1"
     }
   ]
 }
